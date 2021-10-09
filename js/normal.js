8fabf9f6056854ca9a68c82a8db74bdb30eddb18
cad <catx1726@foxmail.com>
2021-10-09 17:02:33 +0800

    update

--- js/normal.js
+++ js/normal.js
@@ -31,7 +31,7 @@ function throttleTwo(fn, delay) {
 
 /* 防抖，在用户停止触发后得 delay 后 执行，N次触发，只执行最后一次 */
 function debounce(fn, delay) {
-  console.log('debounce!',delay)
+  console.log('debounce!', delay)
   let timer = null
   return () => {
     clearTimeout(timer)
@@ -41,3 +41,20 @@ function debounce(fn, delay) {
     }, delay)
   }
 }
+
+const checkTypeEnum = {
+    function: { index: 0, tag: '[object Function]' },
+    number: { index: 1, tag: '[object Number]' },
+    string: { index: 2, tag: '[object String]' },
+    boolean: { index: 3, tag: '[object Boolean]' },
+    object: { index: 4, tag: '[object Object]' }
+  },
+  checkTypes = [checkTypeEnum.function, checkTypeEnum.string]
+function typeValidator(checkTypes, val) {
+  let forInIndex = 0
+  for (let key in checkTypeEnum) {
+    forInIndex++
+    if (checkTypeEnum[key].tag === checkTypes[forInIndex].tag) return checkTypes[forInIndex].tag
+  }
+}
+typeValidator(checkTypes, 0)
