55a5dde51f506e94a874a7b3e1fb4cff9b227b6e
cad <catx1726@foxmail.com>
2023-01-04 16:57:48 +0800

    update

---
+++ 2023/1M/data.js
@@ -0,0 +1,6 @@
+let SourceList = [
+  'FORGOT ROOT PASSWORD',
+  'You may use this option to retrieve a temporary root login if you lost access to this specific VM',
+  'Ubuntu Desktop',
+  'BioShock Infinite'
+]
